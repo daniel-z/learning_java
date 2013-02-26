65f3c43a8c26bc54746a81457a9ab0e26f474395
Daniel Zamorano <dzamorano@tacitknowledge.com>
2013-02-26 16:13:13 -0600

    Improved guess code, implemented more players, basics config and menu.

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,163 +1,100 @@
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-class GuessGameLauncher {
-    public static void main( String[] args){
-        GuessGame guessGame = new GuessGame();
-        guessGame.start();
-    }
+class GameConfig {
+
+     private int rounds = 1;
+     private int numberOfPlayers = 12;
+     private List<String> playerNames = new ArrayList<String>(Arrays.asList("Juan", "Carlos",
+         "Ramiro", "Eve", "Laura", "Florence", "Timmoty", "Rojo", "Green", "Ramon", "Chepina",
+         "Katniss"));
+     private boolean stopAtFirstWin = true;
+
+     int getNumberOfPlayers(){
+         return numberOfPlayers;
+     }
+
+     void setNumberOfPlayers(int newNumberOfPlayers){
+         numberOfPlayers = newNumberOfPlayers;
+     }
+
+     int getNumberOfRounds(){
+         return rounds;
+     }
+
+     void setNumberOfRounds(int newRounds){
+         rounds = newRounds;
+     }
+
+     List<String> getPlayerNames(){
+         return playerNames;
+     }
+
+     void setPlayerNames(List<String> newPlayerNames){
+         playerNames = newPlayerNames;
+     }
+
+     boolean mustStopAtFirstWin(){
+         return stopAtFirstWin;
+     }
+
+     void setStopAtFirstWin(boolean newStopAtFirstWin){
+         stopAtFirstWin = newStopAtFirstWin;
+     }
+
 }
 
-class GuessGame {
+class GameMenu {
 
-    Player player1 = new Player();
-    Player player2 = new Player();
-    Player player3 = new Player();
-    String status  = "game not started yet.";
-    int hiddenNumber = -1;
+    private GameConfig gameConfig;
 
-    void start(){
-        resetGame();
+    public GameMenu ( GameConfig config ){
+        gameConfig = config;
+    }
+
+    void printWelcome(){
+        printLongLine();
         System.out.println("Welcome to the Guess Games!!!");
-        System.out.println("-----------------------------");
-        System.out.println("Today we have 3 participants ...");
+        printLongLine();
+        System.out.println("Today we have "+ gameConfig.getNumberOfPlayers() +" participants ...");
+
+        List<String> players = gameConfig.getPlayerNames();
+        int district = 0;
+
+        for ( String name : players ) {
+            district++;
+            System.out.println("from district "+district+": "+name);
+        }
+
+        printLongLine();
         System.out.println("May the odds be ever in your favor!");
-
-        // starting
-        setHiddenNumber();
-        askForNumberToPlayers();
-        checkForWinners();
-        setGameStatus("first round end");
-
-        System.out.println("-----------------------------");
-        System.out.println(getGameStatus()+"...");
-        System.out.println("-----------------------------");
-        // results
-        System.out.println("Now the results");
-        System.out.println("-----------------------------");
-        System.out.println("THE HIDDEN NUMBER IS...");
-        System.out.println("-----------------------------");
-        System.out.println(getHiddenNumber() + "!!!");
-        System.out.println("-----------------------------");
-        System.out.println("Winners...");
-        System.out.println("-----------------------------");
-        int numberOfWinners = printWinners();
-        System.out.println("we have: "+numberOfWinners+" winners, congratulations.");
-        System.out.println("-----------------------------");
-        System.out.println("Loosers...");
-        System.out.println("-----------------------------");
-        int numberOfLoosers = printLoosers();
-        System.out.println("-----------------------------");
-        System.out.println("we have: "+numberOfLoosers+" loosers, good luck next time.");
-        System.out.println("-----------------------------");
-        System.out.println("The game has ended.");
-
     }
 
-    void resetGame(){
-        status  = "game not started yet.";
-        hiddenNumber = -1;
+    void printDefaultConfig(){
+        printLongLine();
+        System.out.println("Default Game Config:");
+        System.out.println("Number of Rounds: " + gameConfig.getNumberOfRounds());
+        System.out.println("Number of Players: " + gameConfig.getNumberOfPlayers());
+        System.out.println("Player names:" + gameConfig.getPlayerNames());
+        System.out.println("Is the game stopping at first win?: "+gameConfig.mustStopAtFirstWin());
     }
 
-    Integer getRandomNumberBetween0and9(){
-        int randomNumber = (int) (Math.random() * 10);
-        return randomNumber;
+    void printLongLine (){
+        System.out.println("-------------------------------------");
     }
 
-    void setHiddenNumber(){
-        hiddenNumber = getRandomNumberBetween0and9();
+    void printShortLine (){
+        System.out.println("---------------");
     }
-
-    Integer getHiddenNumber(){
-        return hiddenNumber;
-    }
-
-    void askForNumberToPlayers(){
-        player1.guessNumber();
-        player2.guessNumber();
-        player3.guessNumber();
-    }
-
-    void checkForWinners(){
-        int p1guess = player1.getLastGuess();
-        int p2guess = player2.getLastGuess();
-        int p3guess = player3.getLastGuess();
-
-        if( p1guess >= 0 && p1guess == hiddenNumber ){
-            player1.setStatus("winner! :)");
-        }
-        else {
-            player1.setStatus("looser :(");
-        }
-
-        if( p2guess >= 0 && p2guess == hiddenNumber ){
-            player2.setStatus("winner! :)");
-        }
-        else {
-            player2.setStatus("looser :(");
-        }
-
-        if( p3guess >= 0 && p3guess == hiddenNumber ){
-            player3.setStatus("winner! :)");
-        }
-        else {
-            player3.setStatus("looser :(");
-        }
-    }
-
-    void setGameStatus( String newStatus ){
-        status = newStatus;
-    }
-
-    String getGameStatus(){
-        return status;
-    }
-
-    Integer printWinners(){
-        int countWinners = 0;
-        if ( player1.getStatus() == "winner! :)" ){
-            System.out.println("- Player1");
-            countWinners++;
-        }
-
-        if ( player2.getStatus() == "winner! :)" ){
-            System.out.println("- Player2");
-            countWinners++;
-        }
-
-        if ( player3.getStatus() == "winner! :)" ){
-            System.out.println("- Player3");
-            countWinners++;
-        }
-
-        return countWinners;
-    }
-
-
-    Integer printLoosers(){
-        int countLoosers = 0;
-        if ( player1.getStatus() == "looser :(" ){
-            System.out.println("- Player1 lost with:" + player1.getLastGuess());
-            countLoosers++;
-        }
-
-        if ( player2.getStatus() == "looser :(" ){
-            System.out.println("- Player2 lost with:" + player2.getLastGuess());
-            countLoosers++;
-        }
-
-        if ( player3.getStatus() == "looser :(" ){
-            System.out.println("- Player3 lost with:" + player3.getLastGuess());
-            countLoosers++;
-        }
-
-        return countLoosers;
-    }
-
 }
 
 class Player {
     String role = "no role yet.";
     String status = "playing.";
     int lastGuess = -1;
+    String name = "";
 
     Integer guessNumber() {
         int randomNumber = (int) (Math.random() * 10);
@@ -180,9 +117,157 @@ class Player {
     String getStatus(){
         return status;
     }
+
+    void setName( String newName ){
+        name = newName;
+    }
+
+    String getName(){
+        return name;
+    }
     
     Integer getLastGuess(){
         return lastGuess;
     }
 }
+
+class GuessGame {
+
+    private String status  = "game not started yet.";
+    private int hiddenNumber = -1;
+    private GameConfig gameConfig;
+    private GameMenu gameMenu;
+    private Player[] players = new Player[12];
+
+    public GuessGame( GameConfig config, GameMenu menu ){
+        gameConfig = config;
+        gameMenu = menu;
+        createPlayers();
+    }
+
+    void start(){
+        resetGame();
+
+        gameMenu.printWelcome();
+        gameMenu.printDefaultConfig();
+
+        runRound("Number 1");
+    }
+
+    void resetGame(){
+        status  = "game not started yet.";
+        hiddenNumber = -1;
+    }
+
+    private ArrayList<Player> runRound(String roundIdentifier){
+        printLongLine();
+        System.out.println("Starting game...");
+        printLongLine();
+
+        System.out.println("Getting hidden number..0.9.9.8.3.6.4.5.2.5.2.8.3.4.");
+
+        setHiddenNumber();
+
+        System.out.println("done...");
+        System.out.println("Time to get player answers...");
+
+        askForNumberToPlayers();
+
+        System.out.println("All players answered.");
+        System.out.println("Checking results...");
+
+        ArrayList<Player> winners = checkForWinners();
+
+        // print results
+        printLongLine();
+        System.out.println("Round: "+ roundIdentifier +" results:");
+        printLongLine();
+        System.out.println("hidden number: "+ hiddenNumber);
+        printLongLine();
+
+        if (winners != null){
+            System.out.println("The next players won:");
+            for ( Player player : winners ){
+                System.out.println("- "+ player.getName() +"! .. congratulations.");
+            }
+        } else {
+            System.out.println("We didn't get winners.");
+        }
+
+        printLongLine();
+
+        return winners;
+    }
+
+    private void createPlayers(){
+        List<String> playerNames = gameConfig.getPlayerNames();
+        int index = 0;
+        for ( String playerName : playerNames ){
+            players[index] = new Player();
+            players[index].setName( playerName );
+            index++;
+        }
+    }
+
+    private void printLongLine(){
+        System.out.println("--------------------------------------");
+    }
+
+    private void printShortLine(){
+        System.out.println("---------------");
+    }
+
+    private Integer getRandomNumberBetween0and9(){
+        int randomNumber = (int) (Math.random() * 10);
+        return randomNumber;
+    }
+
+    private void setHiddenNumber(){
+        hiddenNumber = getRandomNumberBetween0and9();
+    }
+
+    private Integer getHiddenNumber(){
+        return hiddenNumber;
+    }
+
+    private void askForNumberToPlayers(){
+        for ( Player player : players ){
+            player.guessNumber();
+        }
+    }
+
+    private ArrayList<Player> checkForWinners(){
+        ArrayList<Player> winners = new ArrayList<Player>();
+        int winnerIndex = 0;
+        for ( Player player : players ){
+            int guess = player.getLastGuess();
+            if ( guess == hiddenNumber ){
+                winners.add(player);
+                winnerIndex++;
+            }
+        }
+        return winners;
+    }
+
+    private void setGameStatus( String newStatus ){
+        status = newStatus;
+    }
+
+    private String getGameStatus(){
+        return status;
+    }
+}
+
+
+class GuessGameLauncher {
+    public static void main(String[] args){
+        GameConfig config = new GameConfig();
+        GameMenu menu = new GameMenu(config);
+        GuessGame guessGame = new GuessGame(config, menu);
+        guessGame.start();
+    }
+}
+
+
+
 
